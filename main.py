b92efd29457a520bc2a791c29b6ab91332ca511b
LIU42 <3528865430@qq.com>
2025-08-27 15:49:08 +0800

    优化代码 & 编辑 README.md。

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
+import argparse
+
 from analyzer import LexicalAnalyzer
 
 
-def lexical_analysis(analyzer, source_path, output_path):
-    with open(source_path, 'r') as sources:
-        tokens, errors = analyzer.analysis(sources.readlines())
-
-    with open(output_path, 'w') as outputs:
-        if len(errors) == 0:
-            outputs.writelines(f'{token}\n' for token in tokens)
-        else:
-            outputs.writelines(f'{error}\n' for error in errors)
-
-
-def main():
+def main(args):
     analyzer = LexicalAnalyzer()
-    lexical_analysis(analyzer, 'samples/sample1.txt', 'outputs/output1.txt')
-    lexical_analysis(analyzer, 'samples/sample2.txt', 'outputs/output2.txt')
-    lexical_analysis(analyzer, 'samples/sample3.txt', 'outputs/output3.txt')
-    lexical_analysis(analyzer, 'samples/sample4.txt', 'outputs/output4.txt')
-    lexical_analysis(analyzer, 'samples/sample5.txt', 'outputs/output5.txt')
+
+    source_paths = args.sources
+    output_paths = args.outputs
+
+    for source_path, output_path in zip(source_paths, output_paths):
+        with open(source_path, 'r') as sources:
+            tokens, errors = analyzer.analysis(sources.readlines())
+
+        with open(output_path, 'w') as outputs:
+            if len(errors) == 0:
+                outputs.writelines(f'{token}\n' for token in tokens)
+            else:
+                outputs.writelines(f'{error}\n' for error in errors)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', '--sources', nargs='+', required=True)
+    parser.add_argument('-o', '--outputs', nargs='+', required=True)
+
+    main(parser.parse_args())
